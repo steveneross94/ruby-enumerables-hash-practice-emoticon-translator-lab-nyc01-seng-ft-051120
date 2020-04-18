1fa86824fca009c5c5ae5865c382a7dc2f27ec3f
Steven Ross <ross.steven@me.com>
2020-04-18 17:47:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,16 @@
-
+require 'YAML'
 
 def load_library
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  new_hash = {}
 
+  emoticons.each do |key, value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
